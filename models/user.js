5012c7e533990e0b3d9225c4ab69f957402934a5
VishalChoudhary <vishalghn953@gmail.com>
2024-04-13 21:03:39 +0530

    signup resolve commit

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const { createHmac,randomBytes } = require('crypto');
+
 const userSchema = new mongoose.Schema({
     fullName:{
         type:String,
@@ -13,9 +15,33 @@ const userSchema = new mongoose.Schema({
         type: String,
         required:true,
     },
+    salt:{
+        type:String,
+    },
+    profileImageURL:{
+        type:String,
+        default:("/images/avatar.jpg"),
+    },
+    role:{
+        type:String,
+        enum: ['USER','ADMIN'],
+        default: "USER",
+    },
 },
     {timestamps:true}
 );
+
+userSchema.pre("save", function(next){
+    const user = this;
+    if(!user.isModified("password")) return;
+
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac("sha256",salt).update(user.password).digest("hex");
+
+    this.salt=salt;
+    this.password=hashedPassword;
+    next();
+});
 
 const User = mongoose.model("user", userSchema);
 
